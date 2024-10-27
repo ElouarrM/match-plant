6204b51f6a86253b2abb92ce106773217a191d18
Monir EL OUARROUDI <info.elouarroudi@gmail.com>
2024-10-27 18:55:54 +0100

    HostedVersionss

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,12 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   }
 });
